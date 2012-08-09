1811ec3899f48cddef36d2f41db8c4bd19cfc125
h.naitou <hatsumi.naitou@gmail.com>
2012-08-09 20:31:33 +0900

    scom

--- src/Parson.java
+++ src/Parson.java
@@ -32,7 +32,7 @@ public class Parson {
 渡辺　高志　1.3
 和田　充史　2
 
-public static void sample{
+public static void main(String[] arg){
 	Parson.setLastName("渡辺");
 	Parson.setFirstName("高志");
 	Parson.showName();
