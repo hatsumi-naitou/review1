66439bd4155933a9039d094dca1c28c341c7f477
h.naitou <hatsumi.naitou@gmail.com>
2012-08-09 20:25:33 +0900

    s commit

---
+++ src/Parson.java
@@ -0,0 +1,44 @@
+public class Parson {
+
+	public static String uji = "氏";
+	public static String na = "名";
+
+	public String firstName;
+	public String lastName;
+
+	public String getFirstName(){
+		firstName() = null;
+	}
+
+	public setFirstName(String setFirstName){
+    	firstName = getFirstName();
+	}
+
+	public String getLastName(){
+		lastName() = null;
+	}
+
+	public setLastName(String setLastName){
+		lastName = getLastName();
+	}
+
+	public String showName(){
+		System.out.println("氏：" +  + "　名：" +  + );
+		//氏：ラストネーム＿名：ファーストネーム　をコンソールに表示する
+	}
+
+}
+
+渡辺　高志　1.3
+和田　充史　2
+
+public static void sample{
+	Parson.getLastName("")
+	Parson.getFirstName("")
+	Parson.showName("")
+
+}
+
+
+
+}
